c6f2bd75d8e2d23b7ec6cf147877951f20b16930
Umqambi <umqambi@tuta.io>
2020-02-10 20:52:07 +0300

    Ready for Heroku v2

--- catalogd10/settings.py
+++ catalogd10/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
