b45cfb7c02aa22ab547f57c69d3d69a28da4a7b4
V0land13 <50770856+V0land13@users.noreply.github.com>
2020-03-03 11:07:24 +0300

    Update settings.py
    dell secret key

--- catalogd10/settings.py
+++ catalogd10/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n6$@@9gi#(x)%u#8*r^74s*(@ufm2&0qni(#8!e_yqb4wvnx(e'
+os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
